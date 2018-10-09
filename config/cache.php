6f53a23766a7b09815669dbbbe377999e66b2373
bdbrandt <benjamin.brandt@nau.edu>
2018-10-08 22:17:27 -0500

    Removal of incorrectly committed cache file and changes to gitignore files

--- config/cache.php
+++ config/cache.php
@@ -38,7 +38,7 @@ return [
         ],
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('storage/framework/cache/data'),
+            'path' => storage_path('framework/cache/data'),
         ],
         'memcached' => [
             'driver' => 'memcached',
