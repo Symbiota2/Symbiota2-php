6c3f7e0d0877412ab1847de383fb63868ba41dc9
bdbrandt <benjamin.brandt@nau.edu>
2018-11-11 18:36:56 -0600

    Added ApiResponser and AdminActions files to repo

---
+++ app/Traits/AdminActions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Traits;
+
+trait AdminActions
+{
+	public function before($user, $ability)
+    {
+        /*if ($user->isAdmin()) {
+            return true;
+        }*/
+        return false;
+    }
+}
