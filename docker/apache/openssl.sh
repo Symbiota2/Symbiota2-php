ad0e294451e95369398e592724a65d46d780a365
bdbrandt <benjamin.brandt@nau.edu>
2019-02-08 14:54:34 -0600

    Changed server service from apache to nginx with additional php-fpm container to increase speed in which file changes in bind mount are reflected in containers

--- docker/apache/openssl.sh
+++
@@ -1,3 +0,0 @@
-openssl genrsa -passout pass:1234567891011121314151617181920 -out /home/wwwroot/symbiota2/config/jwt/private.pem -aes256 4096
-
-openssl rsa -passin pass:1234567891011121314151617181920 -in /home/wwwroot/symbiota2/config/jwt/private.pem -out /home/wwwroot/symbiota2/config/jwt/public.pem
