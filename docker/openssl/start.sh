deca12bc86fd5707b12dfb1819a0ccb2d161c69b
bdbrandt <benjamin.brandt@nau.edu>
2019-02-09 08:03:36 -0600

    Added openssl container to generate ssl files

---
+++ docker/openssl/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ ! -f /symbiota2/config/jwt/private.pem ]; then
+    openssl genrsa -passout pass:1234567891011121314151617181920 -out /symbiota2/config/jwt/private.pem -aes256 4096
+    openssl rsa -passin pass:1234567891011121314151617181920 -in /symbiota2/config/jwt/private.pem -out /symbiota2/config/jwt/public.pem
+fi
