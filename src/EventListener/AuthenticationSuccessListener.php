6cf8163448f4dd0359214e9c78aabe74425b8c29
bdbrandt <benjamin.brandt@nau.edu>
2019-04-30 09:15:29 -0500

    Work in progress

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -6,14 +6,17 @@ use Core\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class AuthenticationSuccessListener
 {
     private $em;
+    private $requestStack;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, RequestStack $requestStack)
     {
         $this->em = $em;
+        $this->requestStack = $requestStack;
     }
 
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
@@ -22,15 +25,24 @@ class AuthenticationSuccessListener
         $data = $event->getData();
         $user = $event->getUser();
         $userId = $user->getId();
+        $request = json_decode($this->requestStack->getCurrentRequest()->getContent(),true);
+        $maintainLogin = (array_key_exists('maintainLogin', $request)?$request['maintainLogin']:0);
 
         if (!$user instanceof Users) {
             return;
         }
 
+        if($maintainLogin) {
+            $cookieExp = time() + 31536000;
+        }
+        else {
+            $cookieExp = time() + 9000;
+        }
+
         $cookie = new Cookie(
             'BEARER',
             $data['token'],
-            time() + 63072000
+            $cookieExp
         );
 
         $q = $this->em->createQuery('SELECT ur FROM Core\Entity\UserRoles ur WHERE ur.userId = '.$userId);
@@ -40,6 +52,7 @@ class AuthenticationSuccessListener
         }
 
         $data['id'] = $userId;
+        $data['maintainLogin'] = $maintainLogin;
         $data['firstName'] = $user->getFirstName();
         $data['permissions'] = $permissionArr;
 
---
+++ src/EventSubscriber/JWTCreateSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreateSubscriber implements EventSubscriberInterface
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'lexik_jwt_authentication.on_jwt_created' => ['setJWTExpiration'],
+        ];
+    }
+
+    public function setJWTExpiration(JWTCreatedEvent $event)
+    {
+        $request = json_decode($this->requestStack->getCurrentRequest()->getContent(),true);
+
+        if(array_key_exists('maintainLogin', $request)) {
+            $now = new \DateTime('NOW');
+            if($request['maintainLogin']) {
+                $expiration = date_add($now,date_interval_create_from_date_string("8760 hours"));
+            }
+            else {
+                $expiration = date_add($now,date_interval_create_from_date_string("9000 seconds"));
+            }
+
+            $payload = $event->getData();
+            $payload['maintainLogin'] = $request['maintainLogin'];
+            $payload['exp'] = $expiration->getTimestamp();
+
+            $event->setData($payload);
+        }
+    }
+}
