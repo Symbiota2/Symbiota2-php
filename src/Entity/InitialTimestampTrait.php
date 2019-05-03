e99e42c00955ae0e3524eeb55ee043b21a042af6
curt <Curtis.Dyreson@usu.edu>
2019-05-03 09:21:34 -0600

    Added ApiProperties

---
+++ src/Entity/InitialTimestampTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Core\Entity;
+
+/**
+ * InitialTimestampTrait - This trait implements the initial timestamp.
+ */
+trait InitialTimestampTrait
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="InitialTimeStamp", type="datetime")
+     * @Assert\DateTime
+     */
+    private $initialTimestamp;
+
+
+    public function getInitialTimestamp(): ?\DateTimeInterface
+    {
+        return $this->initialTimestamp;
+    }
+
+    public function setInitialTimestamp(\DateTimeInterface $initialTimestamp)
+    {
+        $this->initialTimestamp = $initialTimestamp;
+
+        //return $this;
+    }
+
+}
