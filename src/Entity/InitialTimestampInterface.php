c4c6d3348513481546227ad8aeabcfda1b547a96
curt <Curtis.Dyreson@usu.edu>
2019-05-03 09:22:08 -0600

    Removed return type from setter, don't think it is used.

--- src/Entity/InitialTimestampInterface.php
+++ src/Entity/InitialTimestampInterface.php
@@ -4,5 +4,5 @@ namespace Core\Entity;
 
 interface InitialTimestampInterface
 {
-    public function setInitialTimestamp(\DateTimeInterface $initialtimestamp): InitialTimestampInterface;
+    public function setInitialTimestamp(\DateTimeInterface $initialtimestamp);
 }
