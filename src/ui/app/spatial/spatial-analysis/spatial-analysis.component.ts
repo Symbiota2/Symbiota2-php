1fa2c26b479604e6ca5e7d2b244c1e3c70a49dfd
bdbrandt <benjamin.brandt@nau.edu>
2019-06-26 19:43:03 -0500

    Spatial plugin development

--- src/ui/app/spatial/spatial-analysis/spatial-analysis.component.ts
+++ src/ui/app/spatial/spatial-analysis/spatial-analysis.component.ts
@@ -1,29 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 import OlMap from 'ol/Map';
-import XYZ from 'ol/source/XYZ';
-import {defaults, FullScreen, Zoom} from 'ol/control';
-import {ZoomSlider, ScaleLine} from 'ol/control';
-import MousePosition from 'ol/control/MousePosition';
-import {createStringXY, format} from 'ol/coordinate';
-import {Heatmap, Tile} from 'ol/layer';
-import {VectorLayer} from 'ol/layer/Vector';
-import {Cluster, OSM, Stamen, Vector, TileImage} from 'ol/source';
-import TileGrid from 'ol/tilegrid/TileGrid';
-import {getTopLeft, getWidth} from 'ol/extent';
-import {fromLonLat, transform, get} from 'ol/proj';
-import OlView from 'ol/View';
-import Projection from 'ol/proj/Projection';
-import {PropertyCluster} from '../../../../../src/ui/assets/js/libraries/PropertyCluster.js';
-import {
-    AtlasManager,
-    Circle,
-    Fill,
-    Stroke,
-    RegularShape,
-    Style
-} from 'ol/style';
-import {Overlay} from 'ol/Overlay';
 
 import {MapService} from '../map.service';
 
@@ -33,6 +10,7 @@ import {MapService} from '../map.service';
     encapsulation: ViewEncapsulation.None,
     styleUrls: [
         '../../../../../node_modules/ol/ol.css',
+        '../../../assets/css/symbiota-ol.css',
         './spatial-analysis.component.css'
     ]
 })
