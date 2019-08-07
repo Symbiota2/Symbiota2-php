817db1a37dc839ec0c88571e97382700b51cdca0
bdbrandt <benjamin.brandt@nau.edu>
2019-08-07 15:14:49 -0500

    Fixed encoding issue with start.sh file for openssl Docker container

--- docker/openssl/start.sh
+++ docker/openssl/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ ! -f /symbiota2/config/jwt/private.pem ]; then
   openssl genrsa -passout pass:1234567891011121314151617181920 -out /symbiota2/config/jwt/private.pem -aes256 4096
   openssl rsa -pubout -passin pass:1234567891011121314151617181920 -in /symbiota2/config/jwt/private.pem -out /symbiota2/config/jwt/public.pem
