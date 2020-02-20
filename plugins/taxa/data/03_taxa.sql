872132b5edb945def6384f0c93764443dc6b513f
bdbrandt <benjamin.brandt@nau.edu>
2020-02-20 15:35:29 -0600

    Corrected default data for Taxa plugin

--- plugins/taxa/data/03_taxa.sql
+++ plugins/taxa/data/03_taxa.sql
@@ -1,6 +1,9 @@
-INSERT INTO taxa(TID, RankId, SciName, UnitName1, SecurityStatus, InitialTimeStamp) VALUES (1, 1, 'Organism', 'Organism', 0, '2018-05-15 12:01:01'),
- (2, 10, 'Monera', 'Monera', 0, '2018-05-15 12:01:01'),
- (3, 10, 'Protista', 'Protista', 0, '2018-05-15 12:01:01'),
+INSERT INTO taxa(TID, RankId, SciName, UnitName1, SecurityStatus, InitialTimeStamp) VALUES
+ (1, 1, 'Organism', 'Organism', 0, '2018-05-15 12:01:01'),
+ (2, 10, 'Bacteria', 'Bacteria', 0, '2018-05-15 12:01:01'),
+ (3, 10, 'Protozoa', 'Protozoa', 0, '2018-05-15 12:01:01'),
  (4, 10, 'Plantae', 'Plantae', 0, '2018-05-15 12:01:01'),
  (5, 10, 'Fungi', 'Fungi', 0, '2018-05-15 12:01:01'),
- (6, 10, 'Animalia', 'Animalia', 0, '2018-05-15 12:01:01')
+ (6, 10, 'Animalia', 'Animalia', 0, '2018-05-15 12:01:01'),
+ (7, 10, 'Chromista', 'Chromista', 0, '2018-05-15 12:01:01'),
+ (8, 10, 'Archaea', 'Archaea', 0, '2018-05-15 12:01:01')
