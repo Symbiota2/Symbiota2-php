592296fb0b5cfd43a5e2d6e92f9adbe548dc3428
Evin Dunn <evindunn14@gmail.com>
2020-09-30 17:51:19 -0700

    Code cleanup on auth.service.ts

--- plugins/symbiota-auth/src/lib/services/auth.service.ts
+++ plugins/symbiota-auth/src/lib/services/auth.service.ts
@@ -1,17 +1,17 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router, NavigationCancel} from '@angular/router';
-import {Observable, BehaviorSubject} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
-import {MatDialog} from '@angular/material';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Router, NavigationCancel} from "@angular/router";
+import {Observable, BehaviorSubject} from "rxjs";
+import {filter, map} from "rxjs/operators";
+import {MatDialog} from "@angular/material";
 import {TranslateService} from "@ngx-translate/core";
 
-import {SpinnerOverlayService} from 'symbiota-shared';
-import {AlertService} from 'symbiota-shared';
-import {LoginComponentService} from './login-component.service';
+import {SpinnerOverlayService} from "symbiota-shared";
+import {AlertService} from "symbiota-shared";
+import {LoginComponentService} from "./login-component.service";
 
-import {AuthData} from '../interfaces/auth-data.interface';
-import {CurrentUser} from '../interfaces/current-user.interface';
+import {AuthData} from "../interfaces/auth-data.interface";
+import {CurrentUser} from "../interfaces/current-user.interface";
 
 const ANONYMOUS_USER: CurrentUser = {
     id: undefined,
@@ -33,6 +33,17 @@ export interface RetrieveLogin {
     providedIn: "root",
 })
 export class AuthService {
+    private static readonly I8N_LOGIN_FAIL = "symbiota-auth.auth-service.login_failed";
+    private static readonly I8N_LOGIN_SENT = "symbiota-auth.auth-service.login_sent";
+    private static readonly I8N_LOGIN_404 = "symbiota-auth.auth-service.no_account_found_email";
+
+    private static readonly I8N_RESET_404 = "symbiota-auth.auth-service.no_account_found";
+    private static readonly I8N_RESET_SUCCESS = "symbiota-auth.auth-service.new_password_sent";
+
+    private static readonly I8N_SESSION_EXPIRED = "symbiota-auth.auth-service.session_expired";
+
+    private static readonly PERM_SUPER_ADMIN = "SuperAdmin";
+
     private subject = new BehaviorSubject<CurrentUser>(undefined);
     private authenticationComplete = false;
     private logoutTimer: any;
@@ -72,120 +83,134 @@ export class AuthService {
         this.authenticateCurrentUser();
     }
 
+    private showSnackbar(text: string, isError = false) {
+        if (isError) {
+            this.alertService.showErrorSnackbar(text, "", 5000);
+        }
+        else {
+            this.alertService.showSnackbar(text, "", 5000);
+        }
+    }
+
     authenticateCurrentUser() {
-        this.http.get<CurrentUser>('/api/users/authuser').subscribe(
-            user => {
-                this.subject.next(user ? user : ANONYMOUS_USER);
-                this.authenticationComplete = true;
-                this.spinnerService.hide();
-            }
-        );
+        this.http.get<CurrentUser>("/api/users/authuser").subscribe(
+            this.onUserAuthenticated);
+    }
+
+    private onUserAuthenticated(user: CurrentUser) {
+        this.subject.next(user ? user : ANONYMOUS_USER);
+        this.authenticationComplete = true;
+        this.spinnerService.hide();
     }
 
     login(username: string, password: string, maintainLogin: number, redirect: string) {
-        const authData: AuthData = { username: username, password: password, maintainLogin: maintainLogin };
+        const authData: AuthData = { username, password, maintainLogin };
         clearTimeout(this.logoutTimer);
         clearTimeout(this.warningTimer);
-        this.http.post<CurrentUser>('/api/login', authData).subscribe(
-            user => {
-                if (!user.maintainLogin) {
-                    this.username = username;
-                    this.password = password;
-                }
-                this.subject.next(user);
-                this.authenticationComplete = true;
-                if (redirect) {
-                    this.router.navigate([redirect]).then(() => {});
-                }
-                this.spinnerService.hide();
-            },
-            () => {
-                this.translate.get('symbiota-auth.auth-service.login_failed').subscribe((text: string) => {
-                    this.spinnerService.hide();
-                    this.alertService.showErrorSnackbar(
-                        text,
-                        '',
-                        5000
-                    );
-                });
-            }
+
+        this.http.post<CurrentUser>("/api/login", authData).subscribe(
+            (user) => this.onLoginSuccess(user, authData, redirect),
+            this.onLoginFailure
         );
     }
 
-    logout() {
+    private onLoginSuccess(user: CurrentUser, authData: AuthData, redirect: string) {
+        if (!user.maintainLogin) {
+            this.username = authData.username;
+            this.password = authData.password;
+        }
+
+        this.subject.next(user);
+        this.authenticationComplete = true;
+        if (redirect) {
+            this.router.navigate([redirect]).then(() => {});
+        }
+
+        this.spinnerService.hide();
+    }
+
+    private onLoginFailure() {
+        this.translate.get(AuthService.I8N_LOGIN_FAIL)
+            .subscribe((text: string) => {
+                this.spinnerService.hide();
+                this.showSnackbar(text, true);
+            });
+    }
+
+    async logout() {
         this.spinnerService.show();
-        this.router.navigate(['/']).then(() => {
-            this.http.get('/api/logout').subscribe(
-                () => {
-                    this.username = '';
-                    this.password = '';
-                    clearTimeout(this.logoutTimer);
-                    clearTimeout(this.warningTimer);
-                    this.subject.next(ANONYMOUS_USER);
-                    this.authenticationComplete = false;
-                    this.spinnerService.hide();
-                }
-            );
-        });
+        await this.router.navigate(["/"]);
+        this.http.get("/api/logout").subscribe(this.onLogout);
+    }
+
+    private onLogout() {
+        this.username = "";
+        this.password = "";
+        clearTimeout(this.logoutTimer);
+        clearTimeout(this.warningTimer);
+        this.subject.next(ANONYMOUS_USER);
+        this.authenticationComplete = false;
+        this.spinnerService.hide();
     }
 
     resetPassword(username: string) {
         const resetData: ResetPassword = { username: username };
-        this.http.post<any>('/api/users/resetpassword', resetData).subscribe(
+        this.http.post<any>("/api/users/resetpassword", resetData)
+            .subscribe((res) => {
+                this.spinnerService.hide();
+
+                if (res.result) {
+                    this.onPasswordResetSuccess();
+                } else {
+                    this.onPasswordResetFailure();
+                }
+            });
+    }
+
+    private onPasswordResetSuccess() {
+        this.translate.get(AuthService.I8N_RESET_SUCCESS)
+            .subscribe(this.showSnackbar);
+    }
+
+    private onPasswordResetFailure() {
+        this.translate.get(AuthService.I8N_RESET_404)
+            .subscribe((text: string) => {
+                this.showSnackbar(text, true);
+            });
+    }
+
+    retrieveLogin(email: string) {
+        const retrieveData: RetrieveLogin = { email: email };
+        this.http.post<any>("/api/users/retrievelogin", retrieveData).subscribe(
             res => {
                 this.spinnerService.hide();
+
                 if (res.result) {
-                    this.translate.get('symbiota-auth.auth-service.new_password_sent').subscribe((text: string) => {
-                        this.alertService.showSnackbar(
-                            text,
-                            '',
-                            5000
-                        );
-                    });
+                    this.onRetrieveLoginSuccess();
                 } else {
-                    this.translate.get('symbiota-auth.auth-service.no_account_found').subscribe((text: string) => {
-                        this.alertService.showErrorSnackbar(
-                            text,
-                            '',
-                            5000
-                        );
-                    });
+                    this.onRetrieveLoginFailure();
                 }
             }
         );
     }
 
-    retrieveLogin(email: string) {
-        const retrieveData: RetrieveLogin = { email: email };
-        this.http.post<any>('/api/users/retrievelogin', retrieveData).subscribe(
-            res => {
-                this.spinnerService.hide();
-                if (res.result) {
-                    this.translate.get('symbiota-auth.auth-service.login_sent').subscribe((text: string) => {
-                        this.alertService.showSnackbar(
-                            text,
-                            '',
-                            5000
-                        );
-                    });
-                } else {
-                    this.translate.get('symbiota-auth.auth-service.no_account_found_email').subscribe((text: string) => {
-                        this.alertService.showErrorSnackbar(
-                            text,
-                            '',
-                            5000
-                        );
-                    });
-                }
-            }
-        );
+    onRetrieveLoginSuccess() {
+        this.translate.get(AuthService.I8N_LOGIN_SENT)
+            .subscribe(this.showSnackbar);
+    }
+
+    onRetrieveLoginFailure() {
+        this.translate.get(AuthService.I8N_LOGIN_404)
+            .subscribe((text: string) => {
+                this.showSnackbar(text, true);
+            });
     }
 
     setWarningTimer(duration: number, dialog: any) {
         if (this.username && this.password) {
             this.warningTimer = setTimeout(() => {
                 this.warningDialog = this.dialog.open(dialog, {
-                    width: '450px',
+                    width: "450px",
                     data: {username: this.username, password: this.password}
                 });
             }, duration * 1000);
@@ -193,19 +218,17 @@ export class AuthService {
     }
 
     setLogoutTimer(duration: number) {
-        this.logoutTimer = setTimeout(() => {
+        this.logoutTimer = setTimeout(async () => {
             if (this.warningDialog) {
                 this.warningDialog.close();
-                this.warningDialog = '';
+                this.warningDialog = "";
             }
-            this.logout();
-            this.translate.get('symbiota-auth.auth-service.session_expired').subscribe((text: string) => {
-                this.alertService.showErrorSnackbar(
-                    text,
-                    '',
-                    5000
-                );
-            });
+
+            await this.logout();
+
+            this.translate.get(AuthService.I8N_SESSION_EXPIRED)
+                .subscribe((text: string) => this.showSnackbar(text, true));
+
         }, duration * 1000);
     }
 
@@ -229,48 +252,65 @@ export class AuthService {
         if (this.authenticationComplete) {
             return this.performPermissionValidation(permissions);
         } else {
-            this.http.get<CurrentUser>('/api/users/authuser').subscribe(
-                user => {
+            this.http.get<CurrentUser>("/api/users/authuser")
+                .subscribe((user) => {
                     this.subject.next(user ? user : ANONYMOUS_USER);
                     this.authenticationComplete = true;
                     return this.performPermissionValidation(permissions);
-                }
-            );
+                });
         }
     }
 
-    performPermissionValidation(permissions: any[]) {
-        let validated = false;
-        if (this.subject.getValue() && this.subject.getValue().id) {
-            const currentPermissions = Object.assign({}, this.subject.getValue().permissions);
-            if (currentPermissions['SuperAdmin']) {
+    performPermissionValidation(requestedPermissions: any[]) {
+        let isValidated = false;
+        const hasSubject = this.getCurrentId() !== 0;
+
+        if (hasSubject) {
+            const userPermissions = Object.assign(
+                {},
+                this.subject.getValue().permissions
+            );
+
+            if (userPermissions[AuthService.PERM_SUPER_ADMIN]) {
                 return true;
-            } else {
-                const currentPermKeys = Object.keys(currentPermissions);
-                permissions.forEach((perm) => {
-                    if (typeof perm === 'string') {
-                        if (currentPermKeys.includes(perm)) {
-                            validated = true;
+            }
+            else {
+                const userPermKeys = Object.keys(userPermissions);
+
+                requestedPermissions.forEach((perm) => {
+                    const permissionType = typeof perm;
+
+                    if (permissionType === "string") {
+                        if (userPermKeys.includes(perm)) {
+                            isValidated = true;
                         }
                     }
-                    if (typeof perm === 'object') {
+                    if (permissionType === "object") {
                         const permKey = Object.keys(perm)[0];
                         const permValue = perm[permKey];
-                        if (currentPermKeys.includes(permKey) && currentPermissions[permKey].includes(permValue)) {
-                            validated = true;
-                        }
+
+                        const hasPermissionKey = userPermKeys.includes(permKey);
+
+                        const permissionValueMatches = (
+                            userPermissions[permKey].includes(permValue)
+                        );
+
+                        isValidated = (
+                            hasPermissionKey && permissionValueMatches
+                        );
                     }
                 });
             }
         }
-        return validated;
+
+        return isValidated;
     }
 
     validateAccess(permissions: any[]) {
         if (this.authenticationComplete) {
             this.performAccessValidation(permissions);
         } else {
-            this.http.get<CurrentUser>('/api/users/authuser').subscribe(
+            this.http.get<CurrentUser>("/api/users/authuser").subscribe(
                 user => {
                     this.subject.next(user ? user : ANONYMOUS_USER);
                     this.authenticationComplete = true;
@@ -280,44 +320,59 @@ export class AuthService {
         }
     }
 
-    performAccessValidation(permissions: any[]) {
-        if (this.subject.getValue() && this.subject.getValue().id) {
+    performAccessValidation(requestedPermissions: any[]) {
+        const hasSubject = this.getCurrentId() !== 0;
+
+        if (hasSubject) {
             let accessGranted = false;
-            const currentPermissions = Object.assign({}, this.subject.getValue().permissions);
-            if (currentPermissions['SuperAdmin']) {
+
+            const userPermissions = { ...this.subject.getValue().permissions };
+            const isSuperUser = userPermissions[AuthService.PERM_SUPER_ADMIN];
+
+            if (isSuperUser) {
                 accessGranted = true;
-                return;
-            } else {
-                const currentPermKeys = Object.keys(currentPermissions);
-                permissions.forEach((perm) => {
-                    if (typeof perm === 'string') {
-                        if (currentPermKeys.includes(perm)) {
-                            accessGranted = true;
-                            return;
-                        }
+            }
+            else {
+                const userPermKeys = Object.keys(userPermissions);
+                requestedPermissions.forEach((perm) => {
+                    const permissionType = typeof perm;
+
+                    if (permissionType === "string") {
+                        accessGranted = userPermKeys.includes(perm);
                     }
-                    if (typeof perm === 'object') {
+
+                    if (permissionType === "object") {
                         const permKey = Object.keys(perm)[0];
                         const permValue = perm[permKey];
-                        if (currentPermKeys.includes(permKey) && currentPermissions[permKey].includes(permValue)) {
-                            accessGranted = true;
-                            return;
-                        }
+
+                        const hasPermissionKey = userPermKeys.includes(permKey);
+
+                        const permissionValueMatches = (
+                            userPermissions[permKey].includes(permValue)
+                        );
+
+                        accessGranted = (
+                            hasPermissionKey && permissionValueMatches
+                        );
                     }
                 });
             }
-            if (accessGranted === false) {
-                this.router.navigate(['/']).then(() => {});
+
+            if (!accessGranted) {
+                this.router.navigate(["/"]);
             }
-        } else {
-            const routerSub = this.router.events.subscribe(val => {
-                if (val instanceof NavigationCancel) {
-                    this.redirectUrl = val.url.toString();
-                }
-            });
-            this.router.navigate(['/']).then(() => {
+        }
+        else {
+            const afterNavigate = this.router.events
+                .subscribe((routerEvent) => {
+                    if (routerEvent instanceof NavigationCancel) {
+                        this.redirectUrl = routerEvent.url.toString();
+                    }
+                });
+
+            this.router.navigate(["/"]).then(() => {
                 this.loginComponentService.openLoginDialog(this.redirectUrl);
-                routerSub.unsubscribe();
+                afterNavigate.unsubscribe();
             });
         }
     }
